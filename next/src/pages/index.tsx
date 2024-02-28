159f9d452f7798d813f4132dceb80a8a72e7d909
hule2343 <aaa402262@gmail.com>
2024-02-28 19:40:07 +0900

    sfa

---
+++ next/src/pages/index.tsx
@@ -0,0 +1,98 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import axios from '@/axios';
+import Link from 'next/link';
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function Home() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    axios({
+      method: "post",
+      url: "/login",
+      data: { username: data.get("username"), password: data.get("password") },
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then((response) => {
+        localStorage.setItem("access_token", response.data.access_token);
+
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="ユーザー名"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+   
+              <Grid item>
+                <Link href="/sign_up" >
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
--- next/src/types/ResponseType.ts
+++ next/src/types/ResponseType.ts
@@ -1,4 +1,4 @@
-import axios from "./axios";
+import axios from "@/axios";
 import { Fetcher } from "swr";
 export type ListProps = {
   url: string;
