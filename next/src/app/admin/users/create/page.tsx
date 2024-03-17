c44eb587975dc39ca656e525de5fae8811d92e2b
hule2343 <aaa402262@gmail.com>
2024-03-17 18:34:01 +0900

    checked admin user form

--- next/src/app/admin/users/create/page.tsx
+++ next/src/app/admin/users/create/page.tsx
@@ -1,29 +1,44 @@
+'use client'
 import * as React from "react";
-import { createTheme } from "@mui/material/styles";
 import axios from "@/axios";
-import { useState } from "react";
 import { AdminUserForm } from "@/components/form/AdminUserForm";
-import { fetcher } from "@/axios";
-import useSWR from "swr";
-import { TaskResponse } from "@/types/ResponseType";
-const theme = createTheme();
-
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 export default function AdminUserCreate() {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        axios
-            .post("/register", {
-                name: data.get("name"),
-                password: data.get("password"),
-                room_number: data.get("room_number"),
-            })
-            .then((response) => {
-            })
-            .catch((err) => {
-            });
-    };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    axios
+      .post("/register", {
+        name: data.get("name"),
+        password: data.get("password"),
+        room_number: data.get("room_number"),
+      })
+      .then((response) => {})
+      .catch((err) => {});
+  };
 
-    return <AdminUserForm handleSubmit={handleSubmit} />;
-};
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          ユーザーを新規作成
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <AdminUserForm handleSubmit={handleSubmit} />
+        </Box>
+      </Box>
+    </Container>
+  );
+}
--- next/src/components/form/AdminUserForm.tsx
+++ next/src/components/form/AdminUserForm.tsx
@@ -6,80 +6,51 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-const theme = createTheme();
 
-
-export const AdminUserForm = ({ handleSubmit }: {
-    handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void,
-
+export const AdminUserForm = ({
+  handleSubmit,
+}: {
+  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
 }) => {
-    return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <Typography component="h1" variant="h5">
-                        ユーザーを新規作成
-                    </Typography>
-                    <Box
-                        component="form"
-                        noValidate
-                        onSubmit={handleSubmit}
-                        sx={{ mt: 3 }}
-                    >
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    autoComplete="given-name"
-                                    name="name"
-                                    required
-                                    fullWidth
-                                    id="name"
-                                    label="名前"
-                                    autoFocus
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="room_number"
-                                    label="部屋番号"
-                                    name="room_number"
-                                    autoComplete="room_number"
-                                />
-                            </Grid>
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            autoComplete="given-name"
+            name="name"
+            required
+            fullWidth
+            id="name"
+            label="名前"
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            fullWidth
+            id="room_number"
+            label="部屋番号"
+            name="room_number"
+            autoComplete="room_number"
+          />
+        </Grid>
 
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    id="password"
-                                    autoComplete="new-password"
-                                />
-                            </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            登録完了
-                        </Button>
-                    </Box>
-                </Box>
-            </Container>
-        </ThemeProvider>
-    );
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            id="password"
+            autoComplete="new-password"
+          />
+        </Grid>
+      </Grid>
+      <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+        登録完了
+      </Button>
+    </>
+  );
 };
