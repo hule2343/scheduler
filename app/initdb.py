f41116c495fd5db7e942271903fe3a079f5166ae
hule2343 <aaa402262@gmail.com>
2024-04-21 16:15:06 +0900

    dockerが動くことを確認

--- app/initdb.py
+++ app/initdb.py
@@ -1,5 +1,16 @@
+from sqlalchemy import create_engine
 from app.models.models import Base
-from app.database import engine
+from app.env import DB_USER, DB_PASSWORD,  DB_NAME
+
+DATABASE = "postgresql+psycopg2://%s:%s@testdb:5432/%s" % (
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME,)
+
+engine = create_engine(
+    DATABASE,
+    echo=True
+)
 
 def initdb():
     Base.metadata.drop_all(bind=engine)
